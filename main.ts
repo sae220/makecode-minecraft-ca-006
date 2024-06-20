99c9b0c48895f246bce7ba6eb34201a991124405
Sae <73706334+sae220@users.noreply.github.com>
2024-06-20 16:16:49 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,7 @@
+namespace agent {
+  //% group="Actions" weight=269
+  //% block="エージェントに前へオークの看板を置かせる"
+  function placeOakSignForward(): void {
+    blocks.place(OAK_SIGN, posAgentCamera(0, 0, 1));
+  }
+}
